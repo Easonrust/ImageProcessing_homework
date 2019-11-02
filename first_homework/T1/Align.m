fe571732a8154b83dde5336d72cfc888d57085a2
Le Yang <36768174+Easonrust@users.noreply.github.com>
2019-11-02 16:09:41 +0800

    fix bugs

--- first_homework/T1/Align.m
+++ first_homework/T1/Align.m
@@ -3,16 +3,9 @@ function aligned = Align(f,g)
 %   此处显示详细说明
 search = 20;
 minMSE = Inf;
-for dx = -search:2:search
-    for dy = -search:2:search
-         frameTform = zeros(size(f));
-%         A = [1 0 dx; 0 1 dy; 0 0 1];
-%         tform = maketform('affine', A.');
-%         [height, width, channels] = size(f);
-%         frameTform = imtransform(f, tform, 'bilinear', ...
-%                                  'XData', [1 width], 'YData', [1 height], ...
-%                                  'FillValues', zeros(channels,1));
-%         
+for dx = -search:search
+    for dy = -search:search
+        frameTform = zeros(size(f));
         frameTform = imtranslate(f, [dx,dy], 'FillValues', zeros(3,1));
 
 
